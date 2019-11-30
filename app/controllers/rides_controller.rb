99f9c3c5c09ee936b9bbebc646222d79a548b02d
Trish Ogen <patriciaogen@gmail.com>
2019-11-30 21:22:02 +0000

    Automatically backed up by Learn

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,10 +1,12 @@
 class RidesController < ApplicationController
-  def create	
-    ride = Ride.create(ride_params)	
-  end	
+  def create
+    ride = Ride.create(ride_params)
+    @user = params[:user_id]
+    redirect_to user_path(@user)
+  end
 
-  private	
-  def ride_params	
-    params.require(:ride).permit(:user_id, :attraction_id)	
+  private
+  def ride_params
+    params.require(:ride).permit(:user_id, :attraction_id)
   end
 end
