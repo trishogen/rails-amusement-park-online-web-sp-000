78046b5ef236b33c9ba1b1542f3fc4997b723df7
Trish Ogen <patriciaogen@gmail.com>
2019-11-30 21:47:50 +0000

    Automatically backed up by Learn

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -20,7 +20,7 @@ class AttractionsController < ApplicationController
   private
 
   def attraction_params
-    params.require(:attraction).permit(:name, :min_height, :nausea_rating, 
+    params.require(:attraction).permit(:name, :min_height, :nausea_rating,
     :happiness_rating, :tickets)
   end
 end
