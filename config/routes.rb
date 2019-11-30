7fc431a163b3d7df075182a523ee14f8d89406b9
Trish Ogen <patriciaogen@gmail.com>
2019-11-30 21:43:31 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
 
   resources :users, only: [:show, :new, :create]
-  resources :attractions, only: [:index, :show]
+  resources :attractions, only: [:index, :show, :new, :create]
   resources :rides, only: [:create]
 
 end
