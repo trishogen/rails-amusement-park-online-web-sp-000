bced683a4efe3502a3cd0ca835c4cd53ce6ffe4c
Trish Ogen <patriciaogen@gmail.com>
2019-11-30 21:20:11 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,8 @@ Rails.application.routes.draw do
   post '/signin', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-
   resources :users, only: [:show, :new, :create]
   resources :attractions, only: [:index, :show]
-
+  resources :rides, only: [:create]
 
 end
